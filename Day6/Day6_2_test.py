ac914960f16d73ea9407e6db0c80fa7cbe4a88b9
Niclas Lindmark <niclas.lindmark@gmail.com>
2018-12-13 19:39:38 +0100

    Update

--- Day6/Day6_2_test.py
+++ Day6/Day6_2_test.py
@@ -2,11 +2,11 @@ import unittest
 import Day6_2
 class Test_TestDay6_2(unittest.TestCase):
     def test_testdata1(self):
-        self.assertEqual(Day6_2.solve("testdata1.txt",32), 17)
+        self.assertEqual(Day6_2.solve("testdata1.txt",32), 16)
     
 
     def test_puzzledata(self):
-        self.assertEqual(Day6_2.solve("puzzledata.txt",10000), 3907)
+        self.assertEqual(Day6_2.solve("puzzledata.txt",10000), 42036)
 
 
 if __name__ == '__main__':
